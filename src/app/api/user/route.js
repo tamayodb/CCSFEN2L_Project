8be9398bbbb8b5fb43ebae377c1c1bdfb92d5685
rbodarve <143065845+rbodarve@users.noreply.github.com>
2025-02-11 01:57:36 +0800

    Added put function in route.js
    Function to save the edited profile to the database

--- src/app/api/user/route.js
+++ src/app/api/user/route.js
@@ -61,3 +61,64 @@ export async function GET(req) {
     });
   }
 }
+
+export async function PUT(req) {
+  if (req.method !== 'PUT') {
+    return new Response(JSON.stringify({ message: 'Method not allowed' }), {
+      status: 405,
+      headers: { 'Content-Type': 'application/json' },
+    });
+  }
+
+  try {
+    await connectToDatabase();
+
+    const token = req.headers.get('authorization')?.split(' ')[1];
+    if (!token) {
+      return new Response(JSON.stringify({ message: 'Unauthorized' }), {
+        status: 401,
+        headers: { 'Content-Type': 'application/json' },
+      });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const userData = await req.json();
+
+    const updatedUser = await Customer.findByIdAndUpdate(
+      decoded.userId,
+      {
+        $set: {
+          username: userData.username,
+          email: userData.email,
+          contact_num: userData.contact_num,
+          address: {
+            street_num: userData.address.street_num,
+            barangay: userData.address.barangay,
+            city: userData.address.city,
+            zip_code: userData.address.zip_code,
+          },
+        },
+      },
+      { new: true }
+    );
+
+    if (!updatedUser) {
+      return new Response(JSON.stringify({ message: 'User not found' }), {
+        status: 404,
+        headers: { 'Content-Type': 'application/json' },
+      });
+    }
+
+    return new Response(JSON.stringify({ message: 'Profile updated successfully' }), {
+      status: 200,
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+  } catch (error) {
+    console.error('Error updating user data:', error);
+    return new Response(JSON.stringify({ message: 'Internal server error' }), {
+      status: 500,
+      headers: { 'Content-Type': 'application/json' },
+    });
+  }
+}
